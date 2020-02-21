3d7b57895de218784f2406fdf5f37acc0b9b2573
Shadow-Archangel <54653220+Shadow-Archangel@users.noreply.github.com>
2020-02-21 20:56:05 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/zeta-vaseraga.jpg') {
+      myImage.setAttribute ('src','images/zeta-vaseraga2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/zeta-vaseraga.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Fighting games are cool, ' + myName;
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Fighting games are cool, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
